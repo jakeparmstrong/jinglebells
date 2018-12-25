57f6a2eafeec92e9ce3675ea4bb55a7dc8c0141e
Jacob <jake.p.armstrong@gmail.com>
2018-12-25 12:11:29 -0500

    Commit before company arrives.
    Next steps:
    
    >Upload video to YouTube/github
    >Fritzing Diagaram
    >Make code less hack-y
    
    Signed-off-by: Jacob <jake.p.armstrong@gmail.com>

--- test3.py
+++ test3.py
@@ -2,7 +2,40 @@
 #JAKE USe PYTHON 3
 import argparse
 import socket
+import RPi.GPIO as GPIO
+import time
 
+#GPIO Pin constants
+C = 8
+D = 25
+E = 24
+F = 23
+G = 18
+A = 15
+B = 14
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(C, GPIO.OUT)
+GPIO.setup(D, GPIO.OUT)
+GPIO.setup(E, GPIO.OUT)
+GPIO.setup(F, GPIO.OUT)
+GPIO.setup(G, GPIO.OUT)
+GPIO.setup(A, GPIO.OUT)
+GPIO.setup(B, GPIO.OUT)
+
+
+def switcher(argument):
+  switchy = {
+  "g" : 8,
+  "a" : 25,
+  "b" : 24,
+  "c" : 23,
+  "d" : 18,
+  "e" : 15,
+  "f" : 14
+  }
+  return switchy.get(argument, "not found")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -29,7 +62,12 @@ def _PrintOscMessages(ip, port):
         stringed_data = str(data)
         octave = stringed_data[2]
         pitch = stringed_data[4]
-        print("Pitch:"+pitch+" | Octave: "+octave)  #debug
+
+        pin = switcher(pitch)
+        GPIO.output(pin, True)
+        time.sleep(0.15)
+        GPIO.output(pin, False)
+        #print("Pitch:"+pitch+" | Octave: "+octave)  #debug
 
 if __name__ == "__main__":
     main()
